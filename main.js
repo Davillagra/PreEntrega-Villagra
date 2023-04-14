213136b207fbf730e1bf026c8c8c68355f565364
Dav <danielsebastianvillagra@hotmail.com>
2023-04-13 21:23:11 -0300

    Segunda Pre-Entrega

--- main.js
+++ main.js
@@ -16,19 +16,87 @@ function validName(isTipe) { // Esta funcion solo determina si lo ingresado no e
     }
     return nam
 }
-function validInt() { // No valida si es o no un entero, solo lo transforma a int
+function validInt() { // No valída si es o no un entero, solo lo transforma a int
     do {
-        int = parseInt(prompt("Cantidad de procutos: "))
+        int = parseInt(prompt("Cantidad de productos: "))
     } while (isNaN(int))
     return int
 }
-let price, prodName, stock
-while (confirm("Quieres cargar productos para la base de datos?")) {
+function printResults() {
+    for (i = 0; i < products.length; i++) {
+        for (const product in products[i]) {
+            console.log( "Id: " + products[i].id +"- Nombre: " + products[i].name + " - Precio: " + products[i].price + " - Stock: " + products[i].stock + " - Tipo: " + products[i].tipe)
+        }
+    }
+}
+function filterTipe(tipe) {
+    for (const product of products) {
+        if (product.tipe == tipe) {
+            console.log("Id: " + products[i].id + "- Nombre: " + products[i].name + " - Precio: " + products[i].price + " - Stock: " + products[i].stock + " - Tipo: " + products[i].tipe)
+        }
+    }
+}
+function orderAB() { // Simplemente ordena alfabeticamente
+    products.sort((a,b)=> {
+        if(a.name > b.name){
+            return 1
+        }
+        if(a.name < b.name){
+            return -1
+        }
+        return 0
+    })
+}
+function orderPrice() {
+    products.sort((a,b)=> a.precio - b.precio)
+}
+function printModResults(modProducts) {
+    for (i = 0; i < modProducts.length; i++) {
+        for (const product in modProducts[i]) {
+            console.log("Id: " + modProducts[i].id + "- Nombre: " + modProducts[i].name + " - Precio: " + modProducts[i].price + " - Stock: " + modProducts[i].stock + " - Tipo: " + modProducts[i].tipe)
+        }
+    }
+}
+
+// Funcion principal
+
+const products = []
+let prodPrice, prodName, prodStock, prodTipe, tipe
+let i = 0
+if (confirm("Quieres cargar productos para la base de datos?")) {
+    do {
+        prodName = validName(false)
+        prodPrice = validNumber()
+        prodStock = validInt()
+        prodTipe = validName(true)
+        i++
+        alert("Id: " + i + "\nProducto: " + prodName + "\nPrecio: " + prodPrice + "\nCantidad: " + prodStock + "\nCategoria: " + prodTipe)
+        products.push({ id: i, name: prodName, price: prodPrice, stock: prodStock, tipe: prodTipe })
+    } while (confirm("Desea agregar otro producto?"))
+//printResults() // No queria sacarlos por alert porque me resulta molesto asi que solo lo saco por consola
+}
+
+if (confirm("Desea realizar otra operacion?")) {
 do {
-    prodName = validName(false)
-    price = validNumber()
-    stock = validInt()
-    tipe = validName(true)
-    alert("Producto: " + prodName + "\nPrecio: " + price + "\nCantidad: " + stock + "\nCategoria: "+ tipe)
-} while (confirm("Desea continuar?"))
+    switch (parseInt(prompt("Que desea hacer con la lista de productos? \nOrdenar alfabeticamente: 1 \nOrdenar por precio: 2 \nFiltrar por categoria: 3"))) {
+        case 1:
+        orderAB()
+        printResults()
+        break
+
+        case 2:
+        products.sort((a,b)=> a.price - b.price)
+        printResults()
+        break
+
+        case 3:
+        tipe = prompt("Categoria: ")
+        printModResults(products.filter((elemento) => elemento.tipe == tipe))
+        break
+
+        default:
+        console.log("Caracter ingresado no valido")
+        break
+    }
+} while (confirm("Volver a la seleccion de operaciones?"))
 }
